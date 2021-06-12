6740e2f198f5219318bac9552b90a429419de913
hyemss <hyemss309@gmail.com>
2021-06-12 19:30:00 +0900

    [CREATE] User, Genre model

---
+++ models/genre.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Genre extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init(
+            {
+                gen_id: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    primaryKey: true,
+                },
+                title: {
+                    type: Sequelize.STRING(10),
+                    allowNull: false,
+                }
+            },
+            {
+                sequelize,
+                timestamp: false,
+                underscored: false,
+                modelName: 'Genre',
+                tableName: 'genres',
+                charset:'utf8',
+                collate: 'utf8_general_ci',
+            }
+        );
+    }
+    static associate(db){
+        db.Genre.belongsToMany(db.User, {throuugh: 'UserGenre'});
+    }
+};
---
+++ models/index.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+const db = {};
+
+const User = require('./user');
+const Genre = require('./genre');
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.User = User;
+db.Genre = Genre;
+
+User.init(sequelize);
+Genre.init(sequelize);
+
+module.exports = db;
