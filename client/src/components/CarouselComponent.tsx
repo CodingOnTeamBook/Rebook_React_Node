7a084bff2a810a9e278b3db5213a531b741b412b
maru5mango <mjel0507@gmail.com>
2021-06-16 15:33:28 +0900

    landingPage carousel

---
+++ client/src/components/CarouselComponent.tsx
@@ -0,0 +1,35 @@
+import React, { FunctionComponent } from 'react';
+import Item from './CarouselItem';
+import styled from 'styled-components';
+import Carousel from 'react-material-ui-carousel';
+import banner_001 from '../style/img/banner_001.png';
+import banner_002 from '../style/img/banner_002.png';
+
+const CarouselContainer = styled.div`
+  margin-top: 50px;
+`;
+
+const CarouselComponent: FunctionComponent = () => {
+  const items = [
+    {
+      id: 1,
+      src: banner_001,
+    },
+    {
+      id: 2,
+      src: banner_002,
+    },
+  ];
+
+  return (
+    <CarouselContainer>
+      <Carousel>
+        {items.map(({ id, src }) => (
+          <Item src={src} key={id} />
+        ))}
+      </Carousel>
+    </CarouselContainer>
+  );
+};
+
+export default CarouselComponent;
--- client/src/pages/LandingPage/index.tsx
+++ client/src/pages/LandingPage/index.tsx
@@ -1,7 +1,25 @@
 import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
+import CarouselComponent from '../../components/CarouselComponent';
+
+const LandingContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const LandingPage: FunctionComponent = () => {
-  return <div>LandingPage</div>;
+  return (
+    <>
+      <LandingContainer>
+        <CarouselComponent />
+        <div>서치 컴포넌트</div>
+        <div>추천 책 컴포넌트</div>
+        <div>인기 리뷰 컴포넌트</div>
+      </LandingContainer>
+    </>
+  );
 };
 
 export default LandingPage;
