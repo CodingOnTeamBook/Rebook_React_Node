881dda690429a9378a41efbcfdce72f5e8208f90
maru5mango <mjel0507@gmail.com>
2021-06-16 15:11:32 +0900

    오타수정

--- client/src/components/common/LoginModalContents.tsx
+++ client/src/components/common/LoginModalContents.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import KakaoLoginBtn from './KakaoLoginBtn';
+import KakaoLoginBtn from './KaKaoLoginBtn';
 import styled from 'styled-components';
 import Logo from '../../style/img/logo.png';
 
