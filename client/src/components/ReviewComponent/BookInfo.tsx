e31783e49bbe5bddcfffcba5edae47855d29b353
Seungmin Lee <reemrev21@gmail.com>
2021-06-18 19:57:50 +0900

    Delete uselsee code

--- client/src/components/ReviewComponent/BookInfo.tsx
+++ client/src/components/ReviewComponent/BookInfo.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent } from 'react';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import Chip from '@material-ui/core/Chip';
 import ReactStars from 'react-rating-stars-component';
