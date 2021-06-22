c30fa1795b936b45ae14f4231f407dc4381f3c1d
Seungmin Lee <reemrev21@gmail.com>
2021-06-22 19:19:34 +0900

    Add like button, fix div so that grid

--- client/src/components/ReviewComponent/ReviewMain.tsx
+++ client/src/components/ReviewComponent/ReviewMain.tsx
@@ -9,17 +9,15 @@ import styled from 'styled-components';
 import CardMedia from '@material-ui/core/CardMedia';
 import Chip from '@material-ui/core/Chip';
 import Favorite from '@material-ui/icons/Favorite';
+import Checkbox from '@material-ui/core/Checkbox';
+import { FavoriteBorder } from '@material-ui/icons';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      marginTop: '30px',
-      flexGrow: 1,
-    },
     paper: {
       display: 'flex',
       padding: theme.spacing(2),
-      cursor: 'pointer',
     },
     cardImg: {
       width: 250,
@@ -37,7 +35,6 @@ const CardContents = styled.div`
   flex-direction: column;
   justify-content: center;
   margin-left: 10px;
-  /* margin-right: 10px; */
 `;
 
 const BookTitle = styled.h2`
@@ -54,25 +51,25 @@ const BookTag = styled.div`
   margin-bottom: 10px;
 `;
 
-// webkit 기반 속성 지원하지 않는 IE에서는 말줄임 작동X
+// webkit 지원하지 않는 IE에서는 말줄임 작동X
 const BookReview = styled.div`
-  margin-bottom: 10px;
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
   -webkit-line-clamp: 4;
   -webkit-box-orient: vertical;
+  cursor: pointer;
 `;
 
 const ReviewUserInfo = styled.div`
   font-weight: bold;
   display: flex;
   flex-direction: row;
+  align-items: center;
 `;
 
 const Like = styled.div`
   margin-left: auto;
-  float: right;
 `;
 
 const ReviewMain: FunctionComponent = () => {
@@ -85,52 +82,47 @@ const ReviewMain: FunctionComponent = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={4}>
-          {/* 페이퍼 클릭하면 디테일 상세 리뷰 모달 동작 */}
-          <Paper className={classes.paper} onClick={ReviewModalOpen}>
-            <div className={classes.cardImg}>
-              <CardMedia
-                className={classes.cover}
-                // 테스트용 입니다.
-                image="https://prodimage.images-bn.com/pimages/9781338311501_p0_v2_s550x406.jpg"
-                title="Live from space album cover"
+    // 3 * 4 = 12
+    <Grid item xs={4} zeroMinWidth>
+      <Paper className={classes.paper}>
+        <div className={classes.cardImg}>
+          <CardMedia
+            className={classes.cover}
+            // 테스트용 입니다.
+            image="https://prodimage.images-bn.com/pimages/9781338311501_p0_v2_s550x406.jpg"
+            title="Live from space album cover"
+          />
+        </div>
+        <CardContents>
+          <BookTitle> 책제목책제목책제목책제목 </BookTitle>
+          <BookTag>
+            <Chip label="#태그" /> <Chip label="#태그" />
+          </BookTag>
+          {/* 리뷰 클릭하면 디테일 상세 리뷰 모달 동작 */}
+          <BookReview onClick={ReviewModalOpen}>
+            리뷰 리뷰 리뷰 리뷰 리뷰 리뷰 리뷰 리뷰 리뷰리뷰 리뷰
+            리뷰리뷰리뷰리뷰리뷰리뷰 리뷰 리뷰 리뷰 리뷰 리뷰 리뷰 리뷰 리뷰
+            리뷰리뷰 리뷰 리뷰리뷰리뷰리뷰리뷰리뷰리뷰 리뷰 리뷰 리뷰 리뷰 리뷰
+            리뷰 리뷰 리뷰리뷰 리뷰 리뷰리뷰리뷰리뷰리뷰리뷰리뷰 리뷰 리뷰 리뷰
+            리뷰 리뷰 리뷰 리뷰 리뷰리뷰 리뷰 리뷰리뷰리뷰리뷰리뷰리뷰리뷰
+          </BookReview>
+          <ReviewUserInfo>
+            by 리북이님
+            <Like>
+              <Checkbox
+                icon={<FavoriteBorder />}
+                checkedIcon={<Favorite />}
+                name="checkedH"
               />
-            </div>
-            <CardContents>
-              <BookTitle> 책제목책제목책제목책제목 </BookTitle>
-              <BookTag>
-                <Chip label="#태그" /> <Chip label="#태그" />
-              </BookTag>
-              <BookReview>
-                리뷰 리뷰 리뷰 리뷰 리뷰 리뷰 리뷰 리뷰 리뷰리뷰 리뷰
-                리뷰리뷰리뷰리뷰리뷰리뷰 리뷰 리뷰 리뷰 리뷰 리뷰 리뷰 리뷰 리뷰
-                리뷰리뷰 리뷰 리뷰리뷰리뷰리뷰리뷰리뷰리뷰 리뷰 리뷰 리뷰 리뷰
-                리뷰 리뷰 리뷰리뷰 리뷰 리뷰리뷰리뷰리뷰리뷰리뷰리뷰 리뷰 리뷰
-                리뷰 리뷰 리뷰 리뷰 리뷰리뷰 리뷰 리뷰리뷰리뷰리뷰리뷰리뷰리뷰
-              </BookReview>
-              <ReviewUserInfo>
-                by 리북이님
-                <Like>
-                  <Favorite color="error"></Favorite>
-                </Like>
-              </ReviewUserInfo>
-            </CardContents>
-          </Paper>
-        </Grid>
-        <Grid item xs={4}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={4}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-      </Grid>
+            </Like>
+          </ReviewUserInfo>
+        </CardContents>
+      </Paper>
       {/* modal */}
       <SimpleModal open={isReviewModalOpen} setOpen={ReviewModalOpen}>
         <ReviewModal handleClose={ReviewModalOpen}></ReviewModal>
       </SimpleModal>
-    </div>
+    </Grid>
   );
 };
 
--- client/src/pages/ReviewPage/index.tsx
+++ client/src/pages/ReviewPage/index.tsx
@@ -1,10 +1,28 @@
 import React, { FunctionComponent } from 'react';
 import ReviewMain from '../../components/ReviewComponent/ReviewMain';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      marginTop: '30px',
+      flexGrow: 1,
+    },
+  })
+);
 
 const ReviewPage: FunctionComponent = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <ReviewMain />
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        {/* 3 * 4 = 12 */}
+        <ReviewMain />
+        <ReviewMain />
+        <ReviewMain />
+      </Grid>
     </div>
   );
 };
