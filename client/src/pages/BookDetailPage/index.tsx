eb07915f553d546a590188b0009f35c55cd35a39
YoujinCha <67622600+youjinDev@users.noreply.github.com>
2021-07-02 16:49:47 +0900

    BookDetail Compoment 마크업

--- client/src/pages/BookDetailPage/index.tsx
+++ client/src/pages/BookDetailPage/index.tsx
@@ -1,7 +1,18 @@
 import React, { FunctionComponent } from 'react';
+import BookDetail from '../../components/BookDetail/BookDetail';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin: 0 auto;
+  min-height: 100vh;
+`;
 
 const BookDetailPage: FunctionComponent = () => {
-  return <div>BookDetailPage</div>;
+  return (
+    <Container>
+      <BookDetail />
+    </Container>
+  );
 };
 
 export default BookDetailPage;
