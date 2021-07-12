ac049e1d09bc204f78a203691a6636c1bc1504a4
Seungmin Lee <reemrev21@gmail.com>
2021-07-12 18:30:13 +0900

    modify review detail

--- client/src/pages/ReviewDetailPage/index.tsx
+++ client/src/pages/ReviewDetailPage/index.tsx
@@ -7,21 +7,22 @@ import Grid from '@material-ui/core/Grid';
 import CommentList from '../../components/ReviewDetailComponent/CommentList';
 
 const ReviewDetailContainer = styled(Grid)`
+  border-radius: 20px;
+  background-color: #e2e2e2;
   margin-top: 50px;
 `;
 
 const BookInfoWrapper = styled(Grid)`
   width: 100%;
-  border-radius: 20px 20px 0 0;
+  border-radius: 20px;
   padding: 5%;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 5px 5px rgba(0, 0, 0, 0.22);
   background-color: ${(props) => props.theme.palette.green};
 `;
 
 const ReviewDetailWrapper = styled(Grid)`
-  border-radius: 0 0 20px 20px;
   width: 100%;
   padding: 3%;
-  background-color: #e2e2e2;
   margin-bottom: 50px;
 `;
 
