69aeec62c4bdab6c3e637070b85cd53979191c40
Seungmin Lee <reemrev21@gmail.com>
2021-07-14 00:36:49 +0900

    [ADD] Myinfo DefaultData

--- client/src/components/myPage/MyInfo.tsx
+++ client/src/components/myPage/MyInfo.tsx
@@ -3,6 +3,12 @@ import styled from 'styled-components';
 import CheckboxesGroup from '../CheckBoxGroupComponent';
 import { genreTags } from '../defaultData/genre';
 import { LineGreenBtn, ProfileImg } from '../../style/componentStyled';
+import {
+  myGender,
+  myAgeRange,
+  myInfo,
+  myProfileImg,
+} from '../../globalFunction/myInfoDefaultValue';
 
 const Container = styled.div`
   display: flex;
@@ -78,36 +84,57 @@ const SubTitle = styled.h2`
 `;
 
 const MyInfo = () => {
+  // 테스트 데이터
+  const USER_DATA = [
+    {
+      id: 2,
+      userId: '1234',
+      nickname: '리북이',
+      genres: '',
+      gender: 'Secret',
+      ageRange: '20대',
+      profileImg: 'defaultImg',
+      info: '안녕하세용~~☺',
+      createdAt: '2021-06-28T08:39:00.108Z',
+      updatedAt: '2021-06-28T08:39:00.162Z',
+    },
+  ];
+
   const imgUrl =
     'https://cdn.pixabay.com/photo/2021/05/17/01/39/iris-6259565_960_720.jpg';
+
   return (
-    <Container>
-      <Title>내 정보</Title>
-      <MainInfo>
-        <MyProfileImg src={imgUrl} />
-        <DisabledArea>
-          <DisabledInfoArea>
-            <label>닉네임</label>
-            <input value="리북이" disabled />
-          </DisabledInfoArea>
-          <DisabledInfoArea>
-            <label>성별</label>
-            <input value="여자" disabled />
-          </DisabledInfoArea>
-          <DisabledInfoArea>
-            <label>나이</label>
-            <input value="10대" disabled />
-          </DisabledInfoArea>
-        </DisabledArea>
-      </MainInfo>
-      <SubTitle>선호하는 장르를 알려주세요</SubTitle>
-      <GenreContainer>
-        <CheckboxesGroup tags={genreTags} />
-      </GenreContainer>
-      <SubTitle>자기 소개</SubTitle>
-      <TextArea placeholder="자기소개를 작성해주세요" />
-      <LineGreenBtn>수정하기</LineGreenBtn>
-    </Container>
+    <>
+      {USER_DATA.map((user) => (
+        <Container key={user.id}>
+          <Title>내 정보</Title>
+          <MainInfo key={user.id}>
+            <MyProfileImg src={myProfileImg(user.profileImg)} />
+            <DisabledArea>
+              <DisabledInfoArea>
+                <label>닉네임</label>
+                <input value={user.nickname} disabled />
+              </DisabledInfoArea>
+              <DisabledInfoArea>
+                <label>성별</label>
+                <input value={myGender(user.gender)} disabled />
+              </DisabledInfoArea>
+              <DisabledInfoArea>
+                <label>나이</label>
+                <input value={myAgeRange(user.ageRange)} disabled />
+              </DisabledInfoArea>
+            </DisabledArea>
+          </MainInfo>
+          <SubTitle>선호하는 장르를 알려주세요</SubTitle>
+          <GenreContainer>
+            <CheckboxesGroup tags={genreTags} />
+          </GenreContainer>
+          <SubTitle>자기 소개</SubTitle>
+          <TextArea placeholder={myInfo(user.info)} />
+          <LineGreenBtn>수정하기</LineGreenBtn>
+        </Container>
+      ))}
+    </>
   );
 };
 
