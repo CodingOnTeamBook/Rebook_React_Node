f42d19781fc76c28f9fd2b05f12e9001cde6121a
YoujinCha <67622600+youjinDev@users.noreply.github.com>
2021-07-17 20:37:13 +0900

    rootReducer에 bookReducer 추가

--- client/src/redux/rootReducer.ts
+++ client/src/redux/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './user/AuthReducer';
 import SearchReducer from './search/reducer';
+import BookReducer from './book/reducer';
 
 const rootReducer = combineReducers({
   auth: AuthReducer,
   search: SearchReducer,
+  book: BookReducer,
 });
 
 export default rootReducer;
