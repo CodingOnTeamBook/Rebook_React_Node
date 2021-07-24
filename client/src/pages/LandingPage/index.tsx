1837056a283cef32196664c6f04bf7c82d2c1cfa
YoujinCha <67622600+youjinDev@users.noreply.github.com>
2021-07-25 02:07:10 +0900

    랜딩페이지 fetch reviews

--- client/src/pages/LandingPage/index.tsx
+++ client/src/pages/LandingPage/index.tsx
@@ -1,5 +1,6 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 import CarouselComponent from '../../components/LandingPage/CarouselComponent';
 import SearchForm from '../../components/common/SearchForm';
@@ -16,13 +17,50 @@ const LandingContainer = styled.main`
 //bestseller랑 populateReview도 여기서 fetch해서 props로 넘겨주는게 낫지만
 //지금은 딱히 문제 될 것 없어 보여서 다른 급한 것 부터 하기
 
+interface review {
+  bookCover: string;
+  bookTitle: string;
+  id: number;
+  like_count: number;
+  score: number;
+  summary: string;
+  tags: Array<string>;
+  writer: string;
+}
+
 const LandingPage: FunctionComponent = () => {
+  const [reviews, setReviews] = useState<Array<review> | null>(null);
+  const [isError, setError] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const response = await axios.get('/api/review/popularity');
+        const {
+          data: { reviews },
+        } = response;
+        if (reviews.length) {
+          setError(false);
+          setReviews(reviews);
+        }
+      } catch (err) {
+        console.log(err);
+        setError(true);
+      }
+    };
+    fetchReviews();
+  }, []);
+
   return (
     <LandingContainer>
       <CarouselComponent />
       <SearchForm />
       <BestSeller />
-      <PopulateReview />
+      {isError ? (
+        <h2>에러가 발생했어요😢 잠시후 다시 시도해주세요</h2>
+      ) : (
+        <PopulateReview reviews={reviews} />
+      )}
     </LandingContainer>
   );
 };
