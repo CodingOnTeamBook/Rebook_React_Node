fdb600d7c225eb7666c76b451eb7ce27d9cee32d
YoujinCha <67622600+youjinDev@users.noreply.github.com>
2021-07-25 02:08:08 +0900

    PopulateReviews.tsx 수정

--- client/src/components/LandingPage/PopulateReviews.tsx
+++ client/src/components/LandingPage/PopulateReviews.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { Container, Header, Main } from '../common/LandingPageCommon';
@@ -7,8 +7,64 @@ import GridLayouot from '../common/GridLayout';
 import GridItem from '../common/GridItem';
 
 const ReviewContainer = styled(Main)``;
+const NoReview = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  margin-top: 0;
+  margin-bottom: 5em;
+  width: 100%;
+  height: 40vh;
+  background: rgb(255, 255, 255);
+  background: radial-gradient(
+    circle,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(241, 241, 241, 1) 100%
+  );
+  border-radius: 12px;
 
-const PopulateReview: FunctionComponent = () => {
+  h1 {
+    margin: 0 auto;
+  }
+`;
+
+interface review {
+  bookCover: string;
+  bookTitle: string;
+  id: number;
+  like_count: number;
+  score: number;
+  summary: string;
+  tags: Array<string>;
+  writer: string;
+}
+
+interface Iprops {
+  reviews: Array<review> | null;
+}
+
+const PopulateReview = ({ reviews }: Iprops) => {
+  console.log(reviews);
+
+  if (!reviews?.length) {
+    return (
+      <Container>
+        <Header>
+          <h2>인기 리뷰</h2>
+          <h3>
+            <Link to="/review">더보기</Link>
+          </h3>
+        </Header>
+        <NoReview>
+          <h1>아직 리뷰가 없어요😅</h1>
+          <h3>리북의 첫 리뷰어가 되어주세요!</h3>
+        </NoReview>
+      </Container>
+    );
+  }
+
   return (
     <Container>
       <Header>
@@ -20,15 +76,11 @@ const PopulateReview: FunctionComponent = () => {
       <ReviewContainer>
         <GridLayouot>
           <>
-            <GridItem>
-              <SmallReview like={false} />
-            </GridItem>
-            <GridItem>
-              <SmallReview like={false} />
-            </GridItem>
-            <GridItem>
-              <SmallReview like={false} />
-            </GridItem>
+            {reviews?.map((review: review, index: number) => (
+              <GridItem key={index}>
+                <SmallReview like={false} review={review} />
+              </GridItem>
+            ))}
           </>
         </GridLayouot>
       </ReviewContainer>
