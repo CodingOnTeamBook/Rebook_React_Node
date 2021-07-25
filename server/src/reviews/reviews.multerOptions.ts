7fa0510d267a62531ad440b3be85e42dfceea7fb
hyemss <hyemss309@gmail.com>
2021-07-25 17:49:19 +0900

    [UPDATE] fs.writeFile .txt -> .html

--- server/src/reviews/reviews.multerOptions.ts
+++ server/src/reviews/reviews.multerOptions.ts
@@ -1,14 +1,40 @@
 import * as multer from 'multer';
 import * as multerS3 from 'multer-s3';
 import * as AWS from 'aws-sdk';
+import * as fs from 'fs';
 
 import { accessKeyId, region, secretAccessKey } from 'src/config/s3.config';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 const s3 = new AWS.S3({
   accessKeyId: accessKeyId,
   secretAccessKey: secretAccessKey,
   region: region,
 });
+
+export const uploadTxt = async (txtfile) => {
+  fs.readFile(`./uploads/${txtfile}`, (err, data) => {
+    if (err) {
+      console.log('error with readFile');
+      return new HttpException('error with readFile', HttpStatus.BAD_REQUEST);
+    }
+    const params = {
+      Bucket: 'rebookbucket',
+      Key: txtfile,
+      Body: JSON.stringify(data, null, 2),
+    };
+    s3.upload(params, (err, data) => {
+      if (err) {
+        console.log(err, err.stack);
+        return new HttpException(
+          'error with upload to S3',
+          HttpStatus.BAD_REQUEST
+        );
+      }
+      return true;
+    });
+  });
+};
 
 const storage = multerS3({
   s3: s3,
