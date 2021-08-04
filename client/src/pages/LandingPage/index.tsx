826eeb7b5698626d59bb0e2a11de7e5a171f883b
YoujinCha <67622600+youjinDev@users.noreply.github.com>
2021-08-04 19:19:46 +0900

    [UPDATE] LandingPage, BookDetailPage에 react-helmet 적용

--- client/src/pages/LandingPage/index.tsx
+++ client/src/pages/LandingPage/index.tsx
@@ -7,6 +7,7 @@ import PopulateReview from '../../components/LandingPage/PopulateReviews';
 import BestSeller from '../../components/LandingPage/BestSeller';
 import fetchData from 'globalFunction/fetchData';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { Helmet } from 'react-helmet';
 
 const LandingContainer = styled.main`
   display: flex;
@@ -96,6 +97,16 @@ const LandingPage: FunctionComponent = () => {
 
   return (
     <LandingContainer>
+      <Helmet>
+        <meta
+          charSet="utf-8"
+          name="Landing Page"
+          content="도서를 검색하고 도서에 대한 리뷰를 공유하는 사이트 ReBook 메인페이지입니다"
+        />
+        <meta name="robots" content="index,follow" />
+        <meta name="keywords" content="ReBook, Landing page, Main Page" />
+        <title>Re:Book</title>
+      </Helmet>
       <CarouselComponent />
       <SearchForm />
       {bestSellerState.isError || reviewsState.isError ? (
